949b8b2ea1399a63f60c433f1608e71029ec0263
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-07-27 00:22:02 +0200

    fix: twitter share link

--- src/lib/components/ShareOnTwitter.svelte
+++ src/lib/components/ShareOnTwitter.svelte
@@ -3,13 +3,14 @@
 	import { IconBrandTwitterFilled } from '@tabler/icons-svelte';
 	export let path: string;
 	export let content: string;
-	$: url = encodeURIComponent(`${env.PUBLIC_SITE_URL}/blog/${path}`);
-	$: text = encodeURIComponent(`"${content}" por @medinamarquezp`);
 </script>
 
 <a
 	class="flex flex-row w-full sm:w-auto font-normal text-white gap-2 justify-center items-center py-4 px-6 rounded-full bg-blue-500 hover-scale"
 	target="_blank"
-	href={`https://twitter.com/share?url=${url}&text=${text}`}
-	><IconBrandTwitterFilled size={22} /> Compártelo en Twitter <span class="hidden sm:inline">💪</span></a
+	href={`https://twitter.com/share?url=${encodeURIComponent(
+		`${env.PUBLIC_SITE_URL}/blog/${path}`
+	)}&text=${encodeURIComponent(`"${content}" por @medinamarquezp`)}`}
+	><IconBrandTwitterFilled size={22} /> Compártelo en Twitter
+	<span class="hidden sm:inline">💪</span></a
 >
