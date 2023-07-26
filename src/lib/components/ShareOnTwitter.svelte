0ae1a058d933256118b9b2adc959b7f517b22ddd
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-07-26 02:20:09 +0200

    feat: share on twitter site url

--- src/lib/components/ShareOnTwitter.svelte
+++ src/lib/components/ShareOnTwitter.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import { env } from '$env/dynamic/public';
 	import { IconBrandTwitterFilled } from '@tabler/icons-svelte';
 	
 	export let path: string;
 	export let content: string;
-	$: url = encodeURIComponent(path);
+	$: url = encodeURIComponent(`${env.PUBLIC_SITE_URL}/${path}`);
 	$: text = encodeURIComponent(`"${content}" por @medinamarquezp`);
 </script>
 
