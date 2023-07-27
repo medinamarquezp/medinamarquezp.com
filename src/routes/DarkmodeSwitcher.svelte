49001e908cdd23fd3baac6cf6452a41379a22959
Pedro Medina Márquez <medinamarquezp@gmail.com>
2023-07-27 10:02:44 +0200

    fix: toogle darkmode icon issue

--- src/routes/DarkmodeSwitcher.svelte
+++ src/routes/DarkmodeSwitcher.svelte
@@ -1,12 +1,25 @@
-<script>
-	import { IconMoon, IconSunLow } from "@tabler/icons-svelte";
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { IconMoon, IconSunLow } from '@tabler/icons-svelte';
+	let theme: string;
+	onMount(() => {
+		theme = localStorage.getItem('theme') || 'cupcake';
+	});
 </script>
-<label class="swap swap-rotate hover-scale btn btn-ghost hover:bg-transparent">
+
+<label class="swap swap-rotate hover-scale btn btn-ghost hover:bg-transparent transition">
 	<input
 		type="checkbox"
 		data-toggle-theme="cupcake,night"
 		data-act-class="ACTIVECLASS"
 	/>
-	<IconSunLow class="swap-on fill-current w-8 h-8" />
-	<IconMoon class="swap-off fill-current w-8 h-8" />
+	{#if theme === 'night'}
+		<IconMoon class="swap-on fill-current w-8 h-8" />
+		<IconSunLow class="swap-off fill-current w-8 h-8" />
+	{:else if theme === 'cupcake'}
+		<IconSunLow class="swap-on fill-current w-8 h-8" />
+		<IconMoon class="swap-off fill-current w-8 h-8" />
+	{:else}
+		&nbsp;
+	{/if}
 </label>
